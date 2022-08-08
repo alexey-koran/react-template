e1d9d0590ff86d47792a2f42f2327bd3b796b0cc
Aleksey Koren <alexey.koran@gmail.com>
2022-08-08 02:35:00 +0200

    Fix svgo configs & commands

---
+++ svgo.config.cjs
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+module.exports = {
+  js2svg: {
+    indent: 2, // string with spaces or number of spaces. 4 by default
+    pretty: true, // boolean, false by default
+  },
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          inlineStyles: {
+            onlyMatchedOnce: false,
+          },
+          removeViewBox: false,
+          mergePaths: false,
+        },
+      },
+    },
+  ],
+};
