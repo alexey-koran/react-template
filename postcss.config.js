6e393f95151347b4c36d11b00b47fda2d4492d6c
Aleksey Koren <alexey.koran@gmail.com>
2022-12-24 20:46:54 +0100

    Run prettier on postcss, tailwindcss configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'stylelint': {},
+    stylelint: {},
     'tailwindcss/nesting': {},
     tailwindcss: {},
     'postcss-preset-env': {
