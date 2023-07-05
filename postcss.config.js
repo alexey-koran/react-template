78c43d325bb29c13137b7e7f9744e8a693ba157b
Aleksey Koren <alexey.koran@gmail.com>
2023-07-05 07:36:19 +0200

    chore: add comment about disabling postcss-preset-env nesting-rules flag

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,9 @@ module.exports = {
     'tailwindcss/nesting': {},
     tailwindcss: {},
     'postcss-preset-env': {
+      // Note that if you are using postcss-preset-env in your project,
+      // you should make sure to disable nesting and let tailwindcss/nesting handle it for you instead
+      // https://tailwindcss.com/docs/using-with-preprocessors#using-post-css-as-your-preprocessor
       features: { 'nesting-rules': false },
     },
     'postcss-assets': {
