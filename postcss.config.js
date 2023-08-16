5ba0fb92a53091d95b7e8d71daf5bf2b062e86c0
Alexey Koran <alexey.koran@gmail.com>
2023-08-16 12:58:29 +0200

    fix: postcss tailwind loading, temp drop stylelint Temporary drop stylelint until they move to ESM

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,22 @@
 import postcssAssets from 'postcss-assets';
 import postcssImport from 'postcss-import';
 import postcssPresetEnv from 'postcss-preset-env';
-import stylelint from 'stylelint';
+// import stylelint from 'stylelint';
 import tailwindcss from 'tailwindcss';
 import tailwindcssNesting from 'tailwindcss/nesting/index.js';
 
-const postCssConfig = {
-  plugins: [
-    postcssImport(),
-    stylelint(),
-    tailwindcssNesting(),
-    tailwindcss(),
-    postcssPresetEnv({
-      // Note that if you are using postcss-preset-env in your project,
-      // you should make sure to disable nesting and let tailwindcss/nesting handle it for you instead
-      // https://tailwindcss.com/docs/using-with-preprocessors#using-post-css-as-your-preprocessor
-      features: { 'nesting-rules': false },
-    }),
-    postcssAssets({
-      loadPaths: ['**'],
-    }),
-  ],
-};
-
-export default postCssConfig;
+export const plugins = [
+  postcssImport(),
+  // stylelint(),
+  tailwindcssNesting(),
+  tailwindcss(),
+  postcssPresetEnv({
+    // Note that if you are using postcss-preset-env in your project,
+    // you should make sure to disable nesting and let tailwindcss/nesting handle it for you instead
+    // https://tailwindcss.com/docs/using-with-preprocessors#using-post-css-as-your-preprocessor
+    features: { 'nesting-rules': false },
+  }),
+  postcssAssets({
+    loadPaths: ['**'],
+  }),
+];
