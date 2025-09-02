35d11442c2d582f32c7a62056c692a2c9cc1b8ff
Alexey Koran <alexey.koran@gmail.com>
2025-09-02 17:47:22 +0400

    chore: add @tailwindcss/postcss package after update tailwindcss to v4 https://tailwindcss.com/docs/upgrade-guide

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/postcss';
 import postcssAssets from 'postcss-assets';
 import postcssPresetEnv from 'postcss-preset-env';
 import stylelint from 'stylelint';
@@ -5,10 +6,8 @@ import stylelint from 'stylelint';
 export default () => ({
   plugins: [
     stylelint(),
+    tailwindcss(),
     postcssPresetEnv({
-      // Note that if you are using postcss-preset-env in your project,
-      // you should make sure to disable nesting and let tailwindcss/nesting handle it for you instead
-      // https://tailwindcss.com/docs/using-with-preprocessors#using-post-css-as-your-preprocessor
       features: { 'nesting-rules': false },
     }),
     postcssAssets({
