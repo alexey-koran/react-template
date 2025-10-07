c0beab9d2d626c077ad36d597ad76d8d8271778c
Alexey Koran <alexey.koran@gmail.com>
2025-10-08 00:25:23 +0400

    fix: TailwindCSS V4 autocompletion via VS Code IntelliSense plugin

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import styles from './App.module.pcss';
 export const App: FC = () => (
   // This is just an example, replace ErrorFallbackExample by your implementation!
   <ErrorBoundary FallbackComponent={ErrorFallbackExample}>
-    <div className={styles.test}>Test</div>
+    <div className={styles.test}>Test PCSS Modules</div>
+    <div className='text-red-300'>Test Tailwind utility classes</div>
   </ErrorBoundary>
 );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,19 +5,6 @@ const tailwindCssConfig = {
     relative: true,
     files: ['./src/**/*.{js,cjs,mjs,jsx,cjsx,mjsx,ts,cts,mts,tsx,ctsx,mtsx}'],
   },
-  theme: {
-    extend: {
-      screens: {
-        xs: { max: '480px' },
-        sm: { max: '576px' },
-        md: { max: '768px' },
-        lg: { max: '992px' },
-        xl: { max: '1200px' },
-        '2xl': { max: '1336px' },
-        '3xl': { max: '1576px' },
-      },
-    },
-  },
   plugins: [],
 } satisfies Config;
 
