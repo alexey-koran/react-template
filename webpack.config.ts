0c9ef92b7a72cc4ae5b3988a6fd712771f6ab1ff
Alexey Koran <alexey.koran@gmail.com>
2025-10-16 22:17:00 +0400

    refactor: webpack.config.ts, add node protocol to import from path & url

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,5 @@
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import type { Configuration as WebpackConfiguration, WebpackOptionsNormalized } from 'webpack';
 
